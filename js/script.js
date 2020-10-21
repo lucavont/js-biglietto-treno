69c9808ebda6e8900fee5ba733995a49252233ee
Luca Cavretti <cavretiluca@live.it>
2020-10-21 18:23:39 +0200

    Price fixed

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ var genBtnEl = document.getElementById('genera-btn');
 
 genBtnEl.addEventListener('click', function() {
     var nameSur = document.getElementById('nome-cognome');
-    var distance = document.getElementById('km-da-percorrere');
+    var distance = document.getElementById('km-da-percorrere').value;
+    console.log(distance);
     var ticketCost = 0.21;
-    var ageDiscount = document.getElementById('fascia-eta');
+    var ageDiscount = document.getElementById('fascia-eta').value;
     var sconto20 = distance * ticketCost * 0.2;
-    var sconto40 = distance * 0.4;
+    var sconto40 = distance * ticketCost * 0.4;
 
-
+    console.log(sconto20);
+    console.log(sconto40);
 
     var ticketName = document.getElementById('nameSurname')
     ticketName.innerHTML = nameSur.value;
@@ -24,7 +26,15 @@ genBtnEl.addEventListener('click', function() {
     document.getElementById("carriage").innerHTML = randomCarrozza;
 
     var ticketPrice = document.getElementById('prezzo');
-    ticketPrice.innerHTML = distance * ticketCost - sconto20;
 
-
+    if (ageDiscount == 'minorenne') {
+        ticketPrice.innerHTML = (distance * ticketCost - sconto20).toFixed(2);
+        document.getElementById('discountCode').innerHTML = "Sconto minorenni 20%";
+    } else if (ageDiscount == 'over65') {
+        ticketPrice.innerHTML = (distance * ticketCost - sconto40).toFixed(2);
+        document.getElementById('discountCode').innerHTML = "Sconto over 65 anni 40%";
+    } else if (ageDiscount == 'altri') {
+        ticketPrice.innerHTML = (distance * ticketCost).toFixed(2);
+        document.getElementById('discountCode').innerHTML = "Nessuno sconto";
+    }
 })
