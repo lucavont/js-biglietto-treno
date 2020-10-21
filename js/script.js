51a33218927c5ebb27108e9e5de53b0fd30a8ae6
Luca Cavretti <cavretiluca@live.it>
2020-10-21 16:33:00 +0200

    Trying to fix

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ genBtnEl.addEventListener('click', function() {
     var nameSur = document.getElementById('nome-cognome');
     var distance = document.getElementById('km-da-percorrere');
     var ageDiscount = document.getElementById('fascia-eta');
-    // var price = distance * 0.21;
-    var sconto20 = distance * 0.2;
+    var sconto20 = distance.value * 0.2;
+    var sconto40 = distance.value * 0.4;
+    var underAge = document.getElementById('under-age').checked;
 
     var ticketName = document.getElementById('nameSurname')
     ticketName.innerHTML = nameSur.value;
@@ -14,10 +15,12 @@ genBtnEl.addEventListener('click', function() {
     var ticketOffer = document.getElementById('discountCode');
     ticketOffer.innerHTML = ageDiscount.value;
 
-    var TicketPrice = document.getElementById('prezzo');
-
     if (ageDiscount = 'under-age') {
+        var TicketPrice = document.getElementById('prezzo');
         TicketPrice.innerHTML = distance.value - sconto20 + "€";
+    } else if (ageDiscount = 'over-age') {
+        var TicketPrice = document.getElementById('prezzo');
+        TicketPrice.innerHTML = distance.value - sconto40 + "€";
     }
 
 })
