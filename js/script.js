cce78494432dca2e871c55346c5a96c3e0a8c59a
Luca Cavretti <cavretiluca@live.it>
2020-10-21 15:10:58 +0200

    Created skeleton and base layout
    - Layout will be fixed as soon as js works

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-var yourAge = prompt('Inserisci la tua età')
-var yourKm = prompt('Inserisci la quantità di km da percorrere')
+var yourAge = document.getElementById('fascia-eta');
+var yourKm = document.getElementById('km-da-percorrere');
+var underAge = document.getElementById('under-age');
+var overAge = document.getElementById('over-age');
 var ticketPrice = yourKm * 0.21;
 var sconto20 = (ticketPrice * 20) / 100;
 var sconto40 = (ticketPrice * 40) / 100;
 
-if (yourAge < 18) {
+if (underAge) {
     ticketPrice = ticketPrice - sconto20;
-} else if (yourAge > 65) {
+} else if (overAge) {
     ticketPrice = ticketPrice - sconto40;
 } else(ticketPrice)
 
